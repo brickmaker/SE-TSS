c17df6e5df3497b9a864c772c14583f7fe2acc01
brickmaker <3150104891@zju.edu.cn>
2018-03-28 16:50:42 +0800

    frontend redux init

--- frontend/src/modules/forum/index.js
+++ frontend/src/modules/forum/index.js
@@ -1,13 +1,40 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {testAction} from "./actions";
 
 class Forum extends Component {
     render() {
+        const {testVal, testAction} = this.props;
         return (
             <div>
-                Forum
+                <h1>Forum</h1>
+                <p>Result: ${testVal}</p>
+                <input
+                    type={'number'}
+                    onKeyDown={(event) => {
+                        console.log(event.target.value);
+                        if (event.keyCode === 13) {
+                            event.preventDefault();
+                            testAction(parseInt(event.target.value))
+                        }
+                    }}
+                />
             </div>
         );
     }
 }
 
-export default Forum;
+const mapStateToProps = (state) => ({
+    testVal: state.forum.testVal
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    testAction: (value) => {
+        dispatch(testAction(value))
+    }
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Forum);
--- frontend/src/top/App.js
+++ frontend/src/top/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import './App.css';
+
+import store from './stores'
 
 import InformationManagement from "../modules/informationManagement";
 import Forum from "../modules/forum";
@@ -8,17 +11,19 @@ import Forum from "../modules/forum";
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">Keep Calm and Just Do It</h1>
-                </header>
-                <BrowserRouter>
-                    <div>
-                        <Route path={'/info'} component={InformationManagement}/>
-                        <Route path={'/forum'} component={Forum}/>
-                    </div>
-                </BrowserRouter>
-            </div>
+            <Provider store={store}>
+                <div className="App">
+                    <header className="App-header">
+                        <h1 className="App-title">Keep Calm and Just Do It</h1>
+                    </header>
+                    <BrowserRouter>
+                        <div>
+                            <Route path={'/info'} component={InformationManagement}/>
+                            <Route path={'/forum'} component={Forum}/>
+                        </div>
+                    </BrowserRouter>
+                </div>
+            </Provider>
         );
     }
 }
