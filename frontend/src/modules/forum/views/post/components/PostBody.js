43063a7dda782d290abd84ab867f749d0c46d24b
brickmaker <3150104891@zju.edu.cn>
2018-05-09 22:32:47 +0800

    add editor style

--- frontend/src/modules/forum/views/post/components/PostBody.js
+++ frontend/src/modules/forum/views/post/components/PostBody.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getPostReplies} from "../actions"
 import Reply from "./Reply"
+import {Button} from "material-ui"
 
 class PostBody extends Component {
     constructor(props) {
@@ -38,17 +39,29 @@ class PostBody extends Component {
                         )
                     })
                 }
-                <div>
-                    pages: {Array.from({length: pageNum}, (v, k) => (k + 1)).map((page) => {
-                    return (
-                        <button
-                            key={page}
-                            onClick={this.clickPageNum}
-                            style={{backgroundColor: page === currPage ? '#ff0000' : '#ffffff'}}>
-                            {page}
-                        </button>
-                    )
-                })}
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row-reverse',
+                    marginBottom: 20
+                }}>
+                    <div>
+                        {Array.from({length: pageNum}, (v, k) => (k + 1)).map((page) => {
+                            return (
+                                <Button
+                                    key={page}
+                                    size={'small'}
+                                    onClick={this.clickPageNum}
+                                    style={{
+                                        minWidth: 30,
+                                        width: 30,
+                                        margin: 2,
+                                        backgroundColor: page === currPage ? '#7986CB' : '#ffffff'
+                                    }}>
+                                    {page}
+                                </Button>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         )
--- frontend/src/modules/forum/views/post/components/editor/PostEditor.js
+++ frontend/src/modules/forum/views/post/components/editor/PostEditor.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 import {EditorState} from 'draft-js';
 import {Editor} from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import {Button} from "material-ui"
+
+const editorStyle = {
+    marginTop: 10,
+    padding: 10,
+    backgroundColor: '#ffffff',
+    height: 300
+}
 
 
 export default class PostEditor extends Component {
@@ -22,12 +30,25 @@ export default class PostEditor extends Component {
     render() {
         const {editorState} = this.state;
         return (
-            <Editor
-                editorState={editorState}
-                wrapperClassName="demo-wrapper"
-                editorClassName="demo-editor"
-                onEditorStateChange={this.onEditorStateChange}
-            />
+            <div style={{
+                paddingBottom: 80,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+            }}>
+                <Editor
+                    editorState={editorState}
+                    // wrapperClassName={editorStyle}
+                    editorStyle={editorStyle}
+                    onEditorStateChange={this.onEditorStateChange}
+                />
+                <Button
+                    style={{margin: 20}}
+                    color={'primary'}
+                    variant={'raised'}
+                    size={'large'}
+                >回帖</Button>
+            </div>
         )
     }
 }
