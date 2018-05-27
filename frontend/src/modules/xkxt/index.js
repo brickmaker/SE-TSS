7304fd93fda2ee465e6b7ff6e103791e2b8e04fd
Lppy <376286605@qq.com>
2018-05-28 02:20:42 +0800

    UI change

--- frontend/src/modules/xkxt/index.js
+++ frontend/src/modules/xkxt/index.js
@@ -1,41 +1,52 @@
 import React from 'react';
 import { connect } from 'react-redux';
-//import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 
-import Button from 'material-ui/Button';
 import Toolbar from 'material-ui/Toolbar';
 import AppBar from 'material-ui/AppBar';
 import Typography from 'material-ui/Typography';
-import IconButton from 'material-ui/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-
-import LeftIcon from '@material-ui/icons/KeyboardArrowLeft';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 import {switchUtility, openFunc} from './actions';
-import ProgramChecking from './modules/programChecking'
-import CourseSearching from './modules/courseSearching'
-import CourseChoosing from './modules/courseChoosing'
-import ScheduleViewing from './modules/scheduleViewing'
-import CourseManagement from './modules/courseManagement'
-
-const styles = {
+import ProgramChecking from './modules/programChecking';
+import CourseSearching from './modules/courseSearching';
+import CourseChoosing from './modules/courseChoosing';
+import ScheduleViewing from './modules/scheduleViewing';
+import CourseManagement from './modules/courseManagement';
+const drawerWidth = 210;
+const styles = theme => ({
   root: {
-    flexGrow: 1,
-  },
-  flex: {
-    flex: 1,
-    textAlign: 'left',
+	flexGrow: 1,
+    //overflow: 'hidden',
+    //position: 'relative',
+    //display: 'flex',
   },
   simpleText: {
-    marginRight: 17,
+    marginLeft: 50,
   },
-  menuButton: {
-  	marginRight: 20,
+  drawerPaper: {
+    position: 'relative',
+	width: drawerWidth,
   },
-};
+  toolbar: theme.mixins.toolbar,
+  divStyle: {
+	  marginLeft: drawerWidth+1,
+	  marginTop: 30,
+  },
+  title: {
+	  padding: "20px",
+	  color: "rgb(174,174,174)",
+  },
+  listItem: {
+	  marginTop: "20px"
+  }
+});
 
-const Xkxt = ({ isAdmin, utility, switchUtility, openDrawer, openFunc, classes }) => {
+const Xkxt = ({ isAdmin, utility, switchUtility, classes }) => {
 	var whatToShow = (<ProgramChecking />);
 	switch(utility){
 		case "培养方案":	
@@ -54,26 +65,47 @@ const Xkxt = ({ isAdmin, utility, switchUtility, openDrawer, openFunc, classes }
 
 	return (
 		<div className={classes.root}>
-			<AppBar position="static">
-				<Toolbar>
-					<IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-						<MenuIcon />
-					</IconButton>
-					<Typography variant="title" color="inherit" className={classes.flex}>
+			<div className={classes.divStyle}>
+				<AppBar position="absolute" style={{left:drawerWidth,right:1}}>
+					<Toolbar>
+						<Typography variant="title" color="inherit" className={classes.simpleText}>
+							{utility}
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				{whatToShow}
+			</div>
+			<Drawer
+				variant="permanent"
+				classes={{
+				paper: classes.drawerPaper,
+			}}>
+				<div className={classes.toolbar}>
+					<Typography variant="title" color="inherit" className={classes.title}>
 						选课系统
 					</Typography>
-					<Typography color="inherit" className={classes.simpleText}>
-						{utility}
-					</Typography>
-					<LeftIcon />
-					<Button color="inherit" onClick={() => switchUtility("培养方案")}>培养方案</Button>
-					<Button color="inherit" onClick={() => switchUtility("课程搜索")}>课程搜索</Button>
-					<Button color="inherit" onClick={() => switchUtility("自主选课")}>自主选课</Button>
-					<Button color="inherit" onClick={() => switchUtility("查看课表")}>查看课表</Button>
-					{isAdmin ? <Button color="inherit" onClick={() => switchUtility("选课管理")}>选课管理</Button> : ''}
-				</Toolbar>
-			</AppBar>
-			{whatToShow}
+				</div>
+				<Divider />
+				<List>
+					<ListItem button onClick={() => switchUtility("培养方案")} className={classes.listItem}>
+						<ListItemText primary="培养方案" />
+					</ListItem>
+					<ListItem button onClick={() => switchUtility("课程搜索")} className={classes.listItem}>
+						<ListItemText primary="课程搜索" />
+					</ListItem>
+					<ListItem button onClick={() => switchUtility("自主选课")} className={classes.listItem}>
+						<ListItemText primary="自主选课" />
+					</ListItem>
+					<ListItem button onClick={() => switchUtility("查看课表")} className={classes.listItem}>
+						<ListItemText primary="查看课表" />
+					</ListItem>
+					{isAdmin ?
+					<ListItem button onClick={() => switchUtility("选课管理")} className={classes.listItem}>
+						<ListItemText primary="选课管理" />
+					</ListItem>
+					:''}
+				</List>			
+			</Drawer>	
 		</div>
 	);
 
@@ -83,67 +115,12 @@ const Xkxt = ({ isAdmin, utility, switchUtility, openDrawer, openFunc, classes }
 const mapStateToProps = (state, props) => ({
 	utility: state.xkxt.utility,
 	isAdmin: state.xkxt.isAdmin,
-	openDrawer: state.xkxt.openDrawer,
 	classes: props.classes
 });
 
 const mapDispatchToProps = (dispatch, props) => ({
 	switchUtility: (utility) => dispatch(switchUtility(utility)),
-	openFunc: () => dispatch(openFunc()),
 });
 
 
 export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(Xkxt));
-
-
-	/*(
-		<MuiThemeProvider>
-			<div>
-				<AppBar title='选课系统' style={{'textAlign': 'left'}}>
-
-				</AppBar>
-
-				<Tabs 
-				tabTemplateStyle={{color:'white',backgroundColor:'white'}} 
-				contentContainerStyle={{color:'white',backgroundColor:'white'}} 
-				style={{
-    flexGrow: 1,
-    backgroundColor: 'theme.palette.background.paper',
-  }}
-				inkBarStyle={{backgroundColor:'#dFdFdF'/*下划线颜色}}
-				indicatorColor="primary"
-				>
-					<Tab label="培养方案" >
-						<ProgramChecking />
-					</Tab>
-					<Tab label="课程搜索" >
-						<CourseSearching />
-					</Tab>
-					<Tab label="自主选课" >
-						<CourseChoosing />
-					</Tab>
-					<Tab label="查看课表" >
-						<ScheduleViewing />
-					</Tab>
-					{isAdmin ? 
-					<Tab label="选课管理" >
-						<CourseManagement />
-					</Tab> : ''}
-				</Tabs>
-			    <Toolbar>
-			    	<ToolbarGroup>
-			    		<ToolbarTitle text='选课系统' />
-			    		<ToolbarSeparator />
-			    	</ToolbarGroup>
-			    	<ToolbarGroup>
-			    		<Button label="培养方案" onClick={() => switchUtility("培养方案")} />
-			    		<Button label="课程搜索" onClick={() => switchUtility("课程搜索")} />
-			    		<Button label="自主选课" onClick={() => switchUtility("自主选课")} />
-			    		<Button label="查看课表" onClick={() => switchUtility("查看课表")} />
-			    		{isAdmin ? <Button label="选课管理" onClick={() => switchUtility("选课管理")} /> : ''}
-			    	</ToolbarGroup>
-			    </Toolbar>
-			    {whatToShow}
-		    </div>
-		</MuiThemeProvider>
-	);*/
