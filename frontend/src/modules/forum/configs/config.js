6eda2d2374ad1da08e60ae91cde0e53d9842afa7
brickmaker <3150104891@zju.edu.cn>
2018-06-13 19:36:52 +0800

    add withtokenheader

--- frontend/src/modules/forum/configs/config.js
+++ frontend/src/modules/forum/configs/config.js
@@ -1,3 +1,3 @@
-export const ROOT_URL = 'http://127.0.0.1:8000'
+export const ROOT_URL = 'http://10.180.113.230:8000'
 export const DEBUG = false
 export const MAX_FILE_SIZE = 100000000
--- frontend/src/modules/forum/views/main/actions.js
+++ frontend/src/modules/forum/views/main/actions.js
@@ -1,4 +1,5 @@
 import {ROOT_URL} from "../../configs/config"
+import {withAuthHeader} from "../../utils/api"
 
 export const GET_SUBSCRIPTIONS = 'get_subscriptions';
 export const GOT_SUBSCRIPTIONS = 'got_subscriptions';
@@ -7,20 +8,28 @@ export const getSubscriptions = (userId) => {
     return dispatch => {
         fetchSubscriptions(userId)
             .then((data) => {
-                if (data.error) {
-                    // todo...
-                }
-                else {
-                    dispatch({
-                        type: GOT_SUBSCRIPTIONS,
-                        subscriptions: data
-                    })
-                }
+                dispatch({
+                    type: GOT_SUBSCRIPTIONS,
+                    subscriptions: data
+                })
+            })
+            .catch(err => {
+                console.log(err)
             })
     }
 };
 
 function fetchSubscriptions(userId) {
-    return fetch(`${ROOT_URL}/api/forum/subscriptions?uid=${userId}`)
-        .then((response) => (response.json()))
+    return fetch(`${ROOT_URL}/api/forum/subscriptions`,
+        {
+            headers: withAuthHeader()
+        }
+    )
+        .then((response) => {
+            if (!response.ok) {
+                console.log(response)
+            } else {
+                return response.json()
+            }
+        })
 }
