757ef70c383972d230450e992af84d19115951b6
cherie <690764871@qq.com>
2018-06-16 11:08:52 +0800

    fix a bug

--- frontend/src/modules/info/components/DropZone.js
+++ frontend/src/modules/info/components/DropZone.js
@@ -3,16 +3,21 @@ import Dropzone from 'react-dropzone';
 import {BACKEND_SERVER_URL, BACKEND_API} from "../config";
 
 class DropZone extends Component{
-
+  constructor(props) {
+        super(props)
+  }
   onDrop(files){
-    const {url_send}= this.props;
-
-    alert(url_send)
+    
+    const {url_send}=this.props;
     var reader = new FormData();
     const file=files[0];
     reader.append("file",file);
     let url=BACKEND_SERVER_URL+ {url_send};
-    
+    var reg = /^.*\.(?:xls|xlsx)$/i;//文件名可以带空格
+    if (!reg.test(file.name)) {//校验不通过
+      alert("请上传excel格式的文件!");
+      return;
+     }
     fetch(url, {
             method: 'POST',
             headers: {
@@ -33,9 +38,12 @@ class DropZone extends Component{
   }
 
   render(){
+    const {url_send}=this.props;
     return(
       <div>
-        <Dropzone onDrop={this.onDrop}>
+        <Dropzone 
+          url_send= {url_send}
+          onDrop={this.onDrop}>
           <div>拖拽文件或点击此处可以上传execel文件</div>
         </Dropzone>
       </div>
