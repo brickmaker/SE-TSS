f388e9985f01a572ae46f882887eededb60b62b8
James Bing <3150104111@zju.edu.cn>
2018-06-19 14:47:22 +0800

    添加成绩审核与录入

--- backend/score_management/views.py
+++ backend/score_management/views.py
@@ -2,10 +2,10 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from score_management.models import Take
-from score_management.models import Score_Relation
-from authentication.models import Course,Student,Faculty,Account,StudentAnalysis
+from score_management.models import Score_Relation,Application
+from authentication.models import Course, Student, Faculty, Account, StudentAnalysis
 from score_management.serializers import TakeSerializer
-from score_management.serializers import ScoreRelationSerializer
+from score_management.serializers import ScoreRelationSerializer,ApplicationSerializer
 from django.db.models import Avg
 from django.db.models import Max
 from django.db.models import Min
@@ -13,94 +13,40 @@ from django.http import JsonResponse
 import json
 from itertools import chain
 
-@api_view(['GET','POST'])
+
+@api_view(['GET', 'POST'])
 def score_list_teacher(request):
     """
     List all student scores according to course id
     """
 
-    #takes = Take.objects.filter(teacher__username=request.data["pid"])
-    #scores=Score_Relation.objects.all()
-    #print(scores[0])
-    score_relations=Score_Relation.objects.filter(course_select_info__course__teacher__username=request.data["pid"])
-    #serializer = TakeSerializer(takes, many=True)
-    serializer=ScoreRelationSerializer(score_relations,many=True)
+    # takes = Take.objects.filter(teacher__username=request.data["pid"])
+    # scores=Score_Relation.objects.all()
+    # print(scores[0])
+    score_relations = Score_Relation.objects.filter(course_select_info__course__teacher__username=request.data["pid"])
+    # serializer = TakeSerializer(takes, many=True)
+    serializer = ScoreRelationSerializer(score_relations, many=True)
     return Response(serializer.data)
 
-        #serializer = TakeSerializer(data=request.data)
-        #if serializer.is_valid():
-        #    serializer.save()
-        #    return Response(serializer.data, status=status.HTTP_201_CREATED)
-        #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET','POST'])
-def test(request):
-    return Response(1)
+    # serializer = TakeSerializer(data=request.data)
+    # if serializer.is_valid():
+    #    serializer.save()
+    #    return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET','POST'])
-def teacher_match(request):
-    """
-    List all student scores according to course id
-    """
-
-    takes = Take.objects.filter(teacher__username=request.data["pid"])
-    match_dict={}
-    #teacher=Faculty.objects.get(username=request.data["pid"])
-    #match_dict["tid"]=takes[0]
-    match_dict["tname"] = takes[0].teacher.name
-
-    match_dict["sid"]=[]
-    match_dict["sname"] = []
-    match_dict["cid"] = []
-    match_dict["cname"] = []
-    for take in takes:
-            #print(take.student.username)
-            #match_dict["sid"].append(take)
-        match_dict["sname"].append(take.student.name)
-            #print(take.course.course_id)
-            #match_dict["cid"].append(take)
-        match_dict["cname"].append(take.course.name)
-    return Response(match_dict)
-
-@api_view(['GET','POST'])
-def student_match(request):
-    """
-    List all student scores according to course id
-    """
-    takes = Take.objects.filter(student__username=request.data["sid"])
-    match_dict={}
-
-    student=Student.objects.get(username=request.data["sid"])
-    match_dict["sid"]=student.username
-    match_dict["sname"] = student.name
-
-    match_dict["tid"]=[]
-    match_dict["tname"] = []
-    match_dict["cid"] = []
-    match_dict["cname"] = []
-    for take in takes:
-        if take.teacher.username not in match_dict["tid"]:
-            print(take.teacher.username)
-            #match_dict["tid"].append(take.teacher.username)
-            #match_dict["tname"].append(take.teacher.name)
-        if take.course.course_id not in match_dict["cid"]:
-            print(take.course.name)
-            #match_dict["cid"].append(take.course.course_id)
-            #match_dict["cname"].append(take.course.name)
-    return Response(match_dict)
-
-@api_view(['GET','POST'])
+@api_view(['GET', 'POST'])
 def score_list_student(request):
     """
     List all student scores according to course id
     """
-
-    takes = Take.objects.filter(student__username=request.data["sid"])
-    serializer = TakeSerializer(takes, many=True)
+    score_relations = Score_Relation.objects.filter(course_select_info__student__username=request.data["sid"])
+    #takes = Take.objects.filter(student__username=request.data["sid"])
+    serializer = ScoreRelationSerializer(score_relations, many=True)
     return Response(serializer.data)
 
-@api_view(['GET',"POST"])
+
+@api_view(['GET', "POST"])
 def insert_score(request):
     """
     For teachers, insert every student score for the first time
@@ -109,48 +55,86 @@ def insert_score(request):
     :param request: Take List
     :return: Information of save state
     """
-    score_relations=Score_Relation.objects.all()
+    score_relations = Score_Relation.objects.all()
     # takes=Take.objects.all()
-    take_list=[]
-    data =request.data["test"]
+    take_list = []
+    data = request.data["test"]
 
     for d in data:
-        score_relation=score_relations.get(course_select_info__student__username_id=d["sid"],
-                                           course_select_info__course__teacher__username_id=d["pid"],
-                                           course_select_info__course__course__course_id=d["cid"],
-                                           test_date=d["test_date"])
+        score_relation = score_relations.get(course_select_info__student__username_id=d["sid"],
+                                             course_select_info__course__teacher__username_id=d["pid"],
+                                             course_select_info__course__course__course_id=d["cid"],
+                                             test_date=d["test_date"])
+        print(score_relation.modify_state)
         if not score_relation.modify_state:
-            score_relation.score=d["score"]
+            score_relation.score = d["score"]
+            score_relation.modify_state = True
         else:
-            return Response("You have to apply !", status=status.HTTP_400_BAD_REQUEST)
+            return Response(False, status=status.HTTP_400_BAD_REQUEST)
         take_list.append(score_relation)
-        #take.save()
+        score_relation.save()
 
-    #serializer=TakeSerializer(data=take_list,many=True)
-    serializer=ScoreRelationSerializer(data=take_list,many=True)
+    # serializer=TakeSerializer(data=take_list,many=True)
+    serializer = ScoreRelationSerializer(data=take_list, many=True)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #
 
-@api_view(['GET',"POST"])
+
+
+@api_view(['GET', "POST"])
 def apply_create(request):
-    pass
+    student=Student.objects.get(username_id=request.data["sid"])
+    teacher=Faculty.objects.get(username_id=request.data["pid"])
+    course=Course.objects.get(course_id=request.data["cid"])
+    application=Application.objects.filter(title=request.data["title"])
+    if not application.exists():
+        application=Application.objects.create(title=request.data["title"],
+                                               course=course,
+                                               teacher=teacher,
+                                               student=student,
+                                               apply_des=request.data["apply_des"])
+        return Response(application.title, status=status.HTTP_201_CREATED)
+    return Response(False)
+
+@api_view(['GET', "POST"])
+def apply_modify(request):
+    application=Application.objects.get(title=request.data["title"])
+    application.state=request.data["state"]
+    application.save()
+    return Response(application.state)
 
 
-@api_view(['GET','POST'])
+
+
+@api_view(['GET', "POST"])
+def apply_list_staff(request):
+    applications=Application.objects.all()
+    serializer=ApplicationSerializer(applications,many=True)
+    return Response(serializer.data)
+
+@api_view(['GET', "POST"])
+def apply_list_teacher(request):
+    applications=Application.objects.filter(teacher__username_id=request.data["pid"])
+    serializer=ApplicationSerializer(applications,many=True)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET', 'POST'])
 def score_statistics(request):
     """
     :param request.data["cid"]
     :return averageScore, medianScore, maxScore, minScore, deviant in a course.
     """
-    scores = Take.objects.filter(course__course_id=request.data["cid"]).values_list('score', flat=True).order_by("score")
+    scores = Take.objects.filter(course__course_id=request.data["cid"]).values_list('score', flat=True).order_by(
+        "score")
     average_score = scores.aggregate(avg_score=Avg('score'))
     count = scores.count()
     if count % 2 == 1:
-        median_score = scores[count//2]
+        median_score = scores[count // 2]
     else:
-        median_score = (scores[count//2-1] + scores[count//2]) / 2
+        median_score = (scores[count // 2 - 1] + scores[count // 2]) / 2
     max_score = scores.aggregate(max_score=Max('score'))
     min_score = scores.aggregate(min_score=Min('score'))
     it = scores.iterator()
@@ -171,14 +155,16 @@ def score_statistics(request):
     resp['deviant'] = deviant
     return JsonResponse(resp)
 
-@api_view(['GET','POST'])
+
+@api_view(['GET', 'POST'])
 def score_distribution(request):
     """
     :param request.data["cid"], request.data["date"]
     :return number of all students in the course and date,
             number of students in each grade interval in the course and date
     """
-    scores = Take.objects.filter(course__course_id=request.data["cid"], test_date=request.data["date"]).values_list('score', flat=True).order_by("score")
+    scores = Take.objects.filter(course__course_id=request.data["cid"], test_date=request.data["date"]).values_list(
+        'score', flat=True).order_by("score")
     count = scores.count()
     it = scores.iterator()
     numList = [0, 0, 0, 0, 0]
@@ -203,14 +189,16 @@ def score_distribution(request):
     resp['[90,100]'] = numList[4]
     return JsonResponse(resp)
 
-@api_view(['GET','POST'])
+
+@api_view(['GET', 'POST'])
 def score_teacher_history(request):
     """
     :param request.data["cid"], request.data["pid"]
     :return number of all students in the course and teacher,
             average grade point in the course and teacher
     """
-    scores = Take.objects.filter(course__course_id=request.data["cid"], teacher_id=request.data["pid"]).values_list('score', flat=True).order_by("score")
+    scores = Take.objects.filter(course__course_id=request.data["cid"], teacher_id=request.data["pid"]).values_list(
+        'score', flat=True).order_by("score")
     it = scores.iterator()
     count = scores.count()
     average_grade_point = 0
@@ -227,14 +215,16 @@ def score_teacher_history(request):
     resp['averageGradePoint'] = average_grade_point
     return JsonResponse(resp)
 
-@api_view(['GET','POST'])
+
+@api_view(['GET', 'POST'])
 def student_gpa_every_year(request):
     """
     :param request.data["sid"]
     :return GPAs of each year
     """
     try:
-        items = Take.objects.filter(student_id=request.data["sid"]).values_list('score', 'test_date').order_by("test_date")
+        items = Take.objects.filter(student_id=request.data["sid"]).values_list('score', 'test_date').order_by(
+            "test_date")
         it = items.iterator()
         resp = {}
         counts = {}
@@ -257,7 +247,8 @@ def student_gpa_every_year(request):
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     return JsonResponse(resp)
 
-@api_view(['GET','POST'])
+
+@api_view(['GET', 'POST'])
 def student_rank(request):
     """
     :param request.data["sid"]
@@ -276,7 +267,8 @@ def student_rank(request):
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     return JsonResponse(resp)
 
-@api_view(['GET','POST'])
+
+@api_view(['GET', 'POST'])
 def update_student_rank(request):
     """
     :param
@@ -309,6 +301,7 @@ def update_student_rank(request):
     except Exception as err:
         print("Exception:", err)
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 def convert_to_grade_point(score):
     if score >= 95:
