02272d40fabd90dbf9a54476e09fea2335a8a66c
Kexiii <kexiii@163.com>
2018-06-20 19:29:14 +0800

    Update cors_origin_whitelist

--- backend/top/settings.py
+++ backend/top/settings.py
@@ -74,7 +74,9 @@ MIDDLEWARE = [
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
-    '*'
+    'null',
+    'localhost:8000',
+    '*',
 )
 
 CORS_ALLOW_METHODS = (
