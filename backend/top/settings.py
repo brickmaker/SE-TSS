2fe5772fb3cfd88bebd646c4ae6a6dc4fd9c98a1
Kexiii <kexiii@163.com>
2018-06-20 18:46:20 +0800

    Update middleware loading order

--- backend/top/settings.py
+++ backend/top/settings.py
@@ -59,6 +59,8 @@ HAYSTACK_CONNECTIONS = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -66,8 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    
 ]
 
 CORS_ALLOW_CREDENTIALS = True
