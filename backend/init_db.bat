40e732f216c6e02e2bdbe8db2dd007b33b86afa3
ShangChingLiu <shangching@live.com>
2018-06-21 14:54:29 +0800

    ignore vim support file

--- backend/init_db.bat
+++ backend/init_db.bat
@@ -2,5 +2,7 @@
 start
 python init_db_1.py
 python init_db_3.py
+python init_db_4.py
 python init_db_5.py
+
 exit
---
+++ backend/init_db.bat~
@@ -0,0 +1,6 @@
+@echo off
+start
+python init_db_1.py
+python init_db_3.py
+python init_db_5.py
+exit
