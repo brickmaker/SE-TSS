36039d7c5ae49e251b9c4c6e4f618f4315fe0bdf
brickmaker <3150104891@zju.edu.cn>
2018-06-22 20:35:47 +0800

    add g6 init

--- backend/reset_migrations.sh
+++ backend/reset_migrations.sh
@@ -9,12 +9,14 @@ rm -r auto_course/migrations/
 rm -r xkxt/migrations/
 rm -r forum/migrations/
 rm -r online_testing/migrations/
+rm -r score_management/migrations/
 
 python3 manage.py makemigrations --empty authentication
 python3 manage.py makemigrations --empty auto_course
 python3 manage.py makemigrations --empty xkxt
 python3 manage.py makemigrations --empty forum
 python3 manage.py makemigrations --empty online_testing
+python3 manage.py makemigrations --empty score_management
 
 echo "make migrations"
 
